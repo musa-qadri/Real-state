024846df8d2a604e0cdcdae3fc28af213b0ef190
Musa Qadri <137062687+musa-qadri@users.noreply.github.com>
2025-06-02 10:45:11 +0500

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,9 +9,9 @@ export default function Hero() {
                     <img src="\Rectangle 1.png" class="h-full right-0  absolute" alt="herobg" />
               
                     <div class='top-44  absolute  ' style={{ display:'flex', flexDirection:'column' , justifyContent:'center'  , width:'100%' }}>
-                    <img src="\heroHeading.png" class=" w-[60%] h-[60%] mx-auto  " alt="herobg" />
+                       <img src="\heroHeading.png" class=" w-[60%] h-[60%] mx-auto  " alt="herobg" />
                        <p class="    text-orange-400 xs:text-xs sm:text-sm">LOREM IPSUM DOLOR SIT AMET CONSECTETUR. CONSEQUAT  </p>
-                       <p  class="    text-orange-400 xs:text-xs sm:text-sm " >NIBH VITAE TELLUS MASSA ACCUMSAN. LEO ODIO</p>
+                       <p  class="    text-orange-400 xs:text-xs sm:text-sm " >NIBH VITAE TELLUS MASSA ACCUMSAN. LEO ODIO </p>
                     </div>
             </div>
                 <Navbar />
